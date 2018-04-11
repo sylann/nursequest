d710c0e6f59b3adc65c51d745b3fa5c8247e8fa0
valentinmele <valmele@hotmail.fr>
2018-04-11 10:22:00 +0200

    Removing useless columns in Students

--- app/models/students.py
+++ app/models/students.py
@@ -8,9 +8,6 @@ class Student(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     degree = db.Column(db.String)
-    logo = db.Column(db.String)
-    tokens = db.Column(db.Integer)
-    description = db.Column(db.String)
 
     id_user = db.Column(db.Integer, db.ForeignKey(User.id, name="fk_assigned_user_id"))
     user = db.relationship('User', lazy='joined')
@@ -21,6 +18,5 @@ class Student(db.Model):
     ideas = db.relationship('Ideas')
 
     def __repr__(self):
-        return "<Student (id='{}, degree='{}', logo='{}', tokens='{}', description='{}', id_user='{}', " \
-               "id_assigned_team='{}')>".format(
-                self.id, self.degree, self.logo, self.tokens, self.description, self.id_user, self.id_assigned_team)
+        return "<Student (id='{}, degree='{}', id_user='{}', id_assigned_team='{}')>".format(
+                self.id, self.degree, self.id_user, self.id_assigned_team)
