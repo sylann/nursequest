2657f8215c8fb6784231811c2a6d999572853a91
Adrien Ceccaldi <adrien.ceccaldi@hotmail.fr>
2018-04-12 01:32:40 +0200

    Add create idea

--- app/views/ideas.py
+++ app/views/ideas.py
@@ -1,12 +1,13 @@
 from pprint import pprint
 
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, session, abort
 from sqlalchemy import or_
 from sqlalchemy.exc import IntegrityError
 import datetime
 
 from app import app, db
 from app.models.ideas import Ideas
+from app.models.students import Student
 
 
 @app.route('/ideas')
@@ -32,11 +33,35 @@ def get_ideas():
     )
 
 
-@app.route('/new_idea')
-def new_idea():
-    return render_template(
-        'new_idea.html',
-        current_route='new_idea',
-        title='Proposez une nouvelle idée de projet',
-        subtitle=''
+@app.route('/ideas/new/')
+def get_create_idea():
+    student = Student.query.filter_by(id_user=session['uid']).first()
+
+    return render_template('create_idea.html',
+                           title='Proposez une nouvelle idée de projet',
+                           data={'student': student}
+                           )
+
+
+@app.route('/ideas/create_idea', methods=['POST'])
+def create_idea():
+    student = Student.query.filter_by(id_user=session['uid']).first()
+    title = request.form.get('title')
+    description = request.form.get('description')
+
+    print(title, description)
+
+    idea = Ideas(
+        id=id,
+        title=title,
+        description=description,
+        id_student=student.id
     )
+
+    db.session.add(idea)
+    try:
+        db.session.commit()
+    except:
+        abort(500)
+
+    return redirect(url_for('get_ideas'))
