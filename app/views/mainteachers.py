9b0b36a378ca8433e8ae4d909b530787fecc9305
Adrien Ceccaldi <adrien.ceccaldi@hotmail.fr>
2018-04-12 18:21:27 +0200

    Fix links, start MainTeacher features

--- app/views/mainteachers.py
+++ app/views/mainteachers.py
@@ -7,35 +7,15 @@ import datetime
 
 from app import app, db
 from app.models.speakers import Speaker
-from app.models.needs import Need
+
 
 @app.route('/mainteacher/dashboard/<int:id>')
 def get_mainteacher_dashboard(id):
     mainteacher = Speaker.query.get(id)
 
-    return render_template('mainteachers/main-teacher-dashboard.html',
-                           data={'mainteacher': mainteacher},
-                           title='Bienvenue ' + mainteacher.user.full_name,
-                           subtitle='Responsable Pédagogique')
-
-@app.route('/mainteachers')
-def get_mainteachers():
-    q = Patient.query
-    page = request.args.get('page', default=1, type=int)
-    searched = request.args.get('search', default='')
-    if searched:
-        q = q.filter(or_(
-            Patient.first_name.ilike('%' + searched + '%'),
-            Patient.last_name.ilike('%' + searched + '%'),
-            Patient.email.ilike('%' + searched + '%'),
-            Patient.social_number.ilike('%' + searched + '%')
-        ))
-    patients = q.paginate(page, 10, False)
     return render_template(
-        'patients.html',
-        current_route='get_patients',
-        title='List of admitted patients',
-        subtitle='',
-        data=patients,
-        searched=searched
-    )
+        'mainteachers/main-teacher-dashboard.html',
+        data={'mainteacher': mainteacher},
+        title='Bienvenue ' + mainteacher.user.full_name,
+        subtitle='Responsable Pédagogique')
+
